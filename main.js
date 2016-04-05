3aab4a24dc0f4c937efe2748152fe7cfefc2678d
Erik <sanchezerik78@yahoo.com>
2016-04-05 09:13:35 -0400

    complete

--- main.js
+++ main.js
@@ -14,25 +14,53 @@ console.log(total);
   var average = total / items.length;
 
 console.log("The average price is",average);
-
+////////////////////////////////////////////
+///////////////////////////////////////////
+//////////////////////////////////////////
 var filtered = items.filter(function(item){
   return item.price >=14 && item.price <=18;
 });
 
 console.log("Items that cost between $14.00 USD and $18.00 USD:",filtered);
-
+////////////////////////////////////////////
+///////////////////////////////////////////
+//////////////////////////////////////////
   var code = items.filter(function(cur){
     return cur.currency_code === "GBP";
   });
 
 console.log("1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18",code);
+////////////////////////////////////////////
+///////////////////////////////////////////
+//////////////////////////////////////////
+  var mat = items.filter(function(element, index, array){
+    if (element.materials.indexOf('wood') >= 0){
+      return true;
+    }else {
+      return false;
+    }
+  })
+  mat.forEach(function(value, index, array){
+    console.log(value.title,' is made of wood.')
+  })
+////////////////////////////////////////////
+///////////////////////////////////////////
+//////////////////////////////////////////
+  var moreMaterials = items.filter(function(element){
+    if (element.materials.length >= 8){
+      return true;
+    }else {
+      return false;
+    }
+  });
 
-//  var mat = items.filter(function(mats){
-//    return (mats.materials) === "wood";
-//  });
-
-//  console.log(mat);
-
+  console.log(moreMaterials);
+  moreMaterials.forEach(function(value){
+    console.log(value.title,' has ', value.materials.length," materials: ", value.materials);
+  });
+  ////////////////////////////////////////////
+  ///////////////////////////////////////////
+  //////////////////////////////////////////
   var sellers = items.filter(function(who){
     return who.who_made === "i_did";
   });
